b067d1b4add7aa873aaf415f8cb4929b4a01bc53
gooooloo <qidu.lin@gmail.com>
2017-08-17 12:16:03 +0800

    save and load

--- train.py
+++ train.py
@@ -2,12 +2,8 @@ import subprocess
 import sys
 import envs
 import numpy as np
-import os
-import tempfile
 import signal
 import tensorflow as tf
-
-import baselines.common.tf_util as U
 
 from baselines import logger
 from baselines.common.schedules import LinearSchedule
@@ -18,14 +14,12 @@ from replay_buffer import ReplayBuffer
 def learn(env,
           render,
           summary_writer,
-          initial_p=1.0,
           lstm_size=32,
           print_freq=1,
-          checkpoint_freq=10000,
-          save_model_freq=None,
-          save_model_func=None,
-          num_cpu=16,
-          callback=None,
+          save_model_freq=10000,
+          save_path=None,
+          save_max_to_keep=10,
+          restore_path=None,
           head_weights=envs.HRA_WEIGHTS,
           num_heads=envs.HRA_NUM_HEADS,
           gamma=envs.HRA_GAMMAS,
@@ -34,14 +28,12 @@ def learn(env,
           buffer_size=10000,
           batch_size=32,
           exploration_fraction=0.1,
+          exploration_initial_eps=1.0,
           exploration_final_eps=0.01,
           train_freq=4,
-          learning_starts=10000,
+          learning_starts=100,
           target_network_update_freq=1000,
           ):
-
-    sess = U.make_session(num_cpu=num_cpu)
-    sess.__enter__()
 
     graph = build_graph.HraDqnGraph(
         ob_shape=env.observation_space.shape,
@@ -56,23 +48,31 @@ def learn(env,
     replay_buffer = ReplayBuffer(buffer_size)
 
     exploration = LinearSchedule(schedule_timesteps=int(exploration_fraction * max_timesteps),
-                                 initial_p=initial_p,
+                                 initial_p=exploration_initial_eps,
                                  final_p=exploration_final_eps)
 
-    # Initialize the parameters and copy them to the target network.
-    U.initialize()
-    graph.update_target()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    episode_rewards = [0.0]
-    saved_mean_reward = None
-    obs = env.reset(graph.get_initial_lstm_state())
-    with tempfile.TemporaryDirectory() as td:
-        model_saved = False
-        model_file = os.path.join(td, "model")
+        if restore_path is not None:
+            saver = tf.train.Saver()
+            saver.restore(sess, restore_path)
+
+        if save_path is not None:
+            saver = tf.train.Saver(max_to_keep=save_max_to_keep)
+
+            def save_model_func(tt):
+                saver.save(sess=sess, save_path=save_path, global_step=tt)
+        else:
+            save_model_func = None
+
+        graph.update_target()
+
+        episode_rewards = [0.0]
+        saved_mean_reward = None
+        obs = env.reset(graph.get_initial_lstm_state())
+
         for t in range(max_timesteps):
-            if callback is not None:
-                if callback(locals(), globals()):
-                    break
             # Take action and update exploration to the newest value
             exp = exploration.value(t)
             action, lstm_state = graph.act_func(obs, True, exp)
@@ -95,8 +95,6 @@ def learn(env,
                     summary.value.add(tag='info/exploration', simple_value=float(exp))
                     summary_writer.add_summary(summary, t)
                     summary_writer.flush()
-
-                print('temp model saved in ', model_file)
 
                 obs = env.reset(graph.get_initial_lstm_state())
                 episode_rewards.append(0.0)
@@ -127,24 +125,9 @@ def learn(env,
                 logger.record_tabular("% time spent exploring", int(100 * exploration.value(t)))
                 logger.dump_tabular()
 
-            if (checkpoint_freq is not None and t > learning_starts and
-                        num_episodes > 100 and t % checkpoint_freq == 0):
-                if saved_mean_reward is None or mean_100ep_reward > saved_mean_reward:
-                    if print_freq is not None:
-                        logger.log("Saving model due to mean reward increase: {} -> {}".format(
-                            saved_mean_reward, mean_100ep_reward))
-                    U.save_state(model_file)
-                    model_saved = True
-                    saved_mean_reward = mean_100ep_reward
-
             if (save_model_freq is not None and save_model_func is not None and
                         t > learning_starts and t % save_model_freq == 0):
                 save_model_func(t)
-
-        if model_saved:
-            if print_freq is not None:
-                logger.log("Restored model with mean reward: {}".format(saved_mean_reward))
-            U.load_state(model_file)
 
         if save_model_func is not None:
             save_model_func(max_timesteps)
@@ -187,6 +170,8 @@ def main():
     learn(
         env=envs.make_env(),
         render=render,
+        save_path='/tmp/hra.sword',
+        restore_path='/tmp/hra.sword-50000',
         summary_writer=summary_writer
     )
 
